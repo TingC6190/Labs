e9e35864fa8fee1601b166c3894e14b7dea1174d
TingC6190 <tingc6190@fullsail.edu>
2015-05-14 15:02:05 -0400

    Lab5
    Commit #6

--- Chen_Ting_Lab5/Chen_Ting_Lab5.js
+++ Chen_Ting_Lab5/Chen_Ting_Lab5.js
@@ -11,45 +11,33 @@ var finalNum;
 var calcSymbol;
 
 // Addition function
-function calcAdd()
+function calcAdd(num1, num2)
 {
-    // ask for 2 numbers
-    num1 = parseFloat(prompt("Enter the first number: "));
-    num2 = parseFloat(prompt("Enter the second number: "));
     finalNum = (num1 + num2);
     return finalNum;
 }
 
 // Subtraction function
-function calcSub()
+function calcSub(num1, num2)
 {
-    // ask for 2 numbers
-    num1 = parseFloat(prompt("Enter the first number: "));
-    num2 = parseFloat(prompt("Enter the second number: "));
     finalNum = (num1 - num2);
     return finalNum;
 }
 
 // Multiplication function
-function calcMult()
+function calcMult(num1, num2)
 {
-    // ask for 2 numbers
-    num1 = parseFloat(prompt("Enter the first number: "));
-    num2 = parseFloat(prompt("Enter the second number: "));
     finalNum = (num1 * num2);
     return finalNum;
 }
 
 // Division function
-function calcDiv()
+function calcDiv(num1, num2)
 {
-    // ask for 2 numbers
-    num1 = parseFloat(prompt("Enter the first number: "));
-    num2 = parseFloat(prompt("Enter the second number: "));
     while ( num2 === 0)
     {
         // if the 2nd number they entered is 0, ask them again for a different number
-        num2 = parseFloat(prompt("Enter a different second number: "));
+        num2 = parseFloat(prompt("Cannot divide by a 0." + "\nEnter a different second number: "));
     }
     finalNum = (num1 / num2);
     return finalNum;
@@ -63,30 +51,33 @@ calcSymbol = parseFloat(prompt("Would you like to add, subtract, multiply, or di
     + "\nEnter 3 for \"Multiplication\""
     + "\nEnter 4 for \"Division\""));
 
+num1 = parseFloat(prompt("Enter the first number"));
+num2 = parseFloat(prompt("Enter the second number"));
+
 // print out the addition problem
 if(calcSymbol == 1)
 {
-    calcAdd();
+    calcAdd(num1, num2);
     console.log(num1 + " + " + num2 + " = " + finalNum);
 }
 
 // print out the subtraction problem
 else if (calcSymbol == 2)
 {
-    calcSub();
+    calcSub(num1, num2);
     console.log(num1 + " - " + num2 + " = " + finalNum);
 }
 
 // print out the multiplication problem
 else if (calcSymbol == 3)
 {
-    calcMult();
+    calcMult(num1, num2);
     console.log(num1 + " * " + num2 + " = " + finalNum);
 }
 
 // print out the division problem
 else if (calcSymbol == 4)
 {
-    calcDiv();
+    calcDiv(num1, num2);
     console.log(num1 + " / " + num2 + " = " + finalNum);
 }
