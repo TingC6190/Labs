d5ff845e65bf2738b21483d97bbf9775c8ffef89
TingC6190 <tingc6190@fullsail.edu>
2015-05-16 13:28:51 -0400

    Lab6
    Commit #1

---
+++ Chen_Ting_Lab6/Chen_Ting_Lab6.js
@@ -0,0 +1,24 @@
+// Ting Chen
+// May 16th 2015
+// SDI 1505
+// Lab6
+
+// alert("JavaScript works!");
+
+// GLOBAL VARIABLES
+
+
+// FUNCTIONS
+function finalName()
+{
+    var teamState = [];
+    var teamName = [];
+
+    for (var i = 0; i < teamState.length; i++)
+    {
+
+    }
+}
+
+// MAIN CODE
+finalName();
