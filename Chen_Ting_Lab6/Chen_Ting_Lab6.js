81215d9afdd36d8f801c6afc3199d30a5a6878ac
TingC6190 <tingc6190@fullsail.edu>
2015-05-16 13:47:12 -0400

    Lab6
    Commit #4

--- Chen_Ting_Lab6/Chen_Ting_Lab6.js
+++ Chen_Ting_Lab6/Chen_Ting_Lab6.js
@@ -9,23 +9,41 @@
 
 
 // FUNCTIONS
+// this function creates two arrays which are then concatenated to form a basketball team name
 function finalName()
 {
+    // array for team state
     var teamState = ["Orlando", "Los Angeles", "New York", "Chicago", "Miami"];
+    // array for team name
     var teamName = ["Magic", "Lakers", "Knicks", "Bulls", "Heats"];
 
+    // loop that combines both arrays
     for (var i = 0; i < teamState.length; i++)
     {
         console.log("Team #" + (i+1) + ": " + teamState[i] + " " + teamName[i]);
     }
 
+    // pushing in another teamState/teamName
     teamState.push("Cleveland");
-    teamName.push("Cavaliers")
+    teamName.push("Cavaliers");
 
+    console.log("\nThis is the result after pushing in another team.");
+    // loop with the newly added team
     for (var i = 0; i < teamState.length; i++)
     {
         console.log("Team #" + (i+1) + ": " + teamState[i] + " " + teamName[i]);
     }
+
+    // shifting out a team
+    teamState.shift();
+    teamName.shift();
+
+    console.log("\nThis is the result after shifting out a team.")
+    for (var i = 0; i < teamState.length; i++)
+    {
+        console.log("Team #" + (i+1) + ": " + teamState[i] + " " + teamName[i]);
+    }
+
 }
 
 // MAIN CODE
