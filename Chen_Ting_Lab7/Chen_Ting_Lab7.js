b44037f914ea635941c6d2d04bdea51792a0a1b7
TingC6190 <tingc6190@fullsail.edu>
2015-05-19 13:04:34 -0400

    Lab 7
    Commit #1

---
+++ Chen_Ting_Lab7/Chen_Ting_Lab7.js
@@ -0,0 +1,17 @@
+//alert("JavaScript works!");
+
+// Ting Chen
+// May 19th 2015
+// SDI 1505
+// Lab 7
+
+// global variables
+var arrayOne = [10, 6, 7, 3, 12, 5];
+var arrayTwo = [25, 100, 4, 22, 16, 105, 2, 7];
+var arrayThree = [2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42];
+var finalArray = [];
+
+//function
+
+
+//main code
