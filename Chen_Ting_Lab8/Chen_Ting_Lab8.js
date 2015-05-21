265e02c07efa34a3bb52b519a9e8e0e6026f9dae
TingC6190 <tingc6190@fullsail.edu>
2015-05-21 15:38:15 -0400

    Lab 8 Commit#1

---
+++ Chen_Ting_Lab8/Chen_Ting_Lab8.js
@@ -0,0 +1,20 @@
+// alert("JavaScript works!");
+
+// Ting Chen
+// May 21st 2015
+// SDI 1505
+// Lab 8
+
+
+
+// Global Variables
+var emailString = "";
+
+// Function
+function checkEmail(emailArg)
+{
+    
+}
+
+// Main code
+checkEmail(emailString);
