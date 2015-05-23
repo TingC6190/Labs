cd4d451dcca1c9bff14c817fb13c019744361c4a
TingC6190 <tingc6190@fullsail.edu>
2015-05-23 14:31:16 -0400

    Lab 9 Commit #2

--- Chen_Ting_Lab9/Chen_Ting_Lab9.js
+++ Chen_Ting_Lab9/Chen_Ting_Lab9.js
@@ -6,10 +6,11 @@
 
 // Global Variables
 var finalNum;
+var finalString;
 
 
 // Problem 1
-// Functions
+// Function
 function formatNum(firstNum, decimalNum)
 {
     // local variable
@@ -21,6 +22,33 @@ function formatNum(firstNum, decimalNum)
     return priorNum;
 }
 
+
+
+
+// Problem 2
+// Function
+function stringToNum(stringArg)
+{
+    // ask for a number string
+    var stringArg = prompt("Enter a number string to be converted into a number:", "23")
+
+    // if it's not a number, ask again
+    while (isNaN(stringArg))
+    {
+        stringArg = prompt("Enter a valid number string: ", "23");
+    }
+    // print out number if it is a number string
+    if (!(isNaN(stringArg)))
+    {
+        stringArg = parseFloat(stringArg);
+    }
+
+    return stringArg;
+}
+
 // Main code
 finalNum = formatNum(3.141592654, 5);
 console.log("Final number: " + finalNum);
+
+finalString = stringToNum();
+console.log ("This is your number string that is converted into a number: " + finalString);
