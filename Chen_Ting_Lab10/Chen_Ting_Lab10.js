b61593d20ac32d745cd70e5e6201a217b170afc6
TingC6190 <tingc6190@fullsail.edu>
2015-05-26 12:34:27 -0400

    Lab 10 Commit #1

---
+++ Chen_Ting_Lab10/Chen_Ting_Lab10.js
@@ -0,0 +1,21 @@
+// alert("JavaScript works!");
+
+// Ting Chen
+// SDI 1505
+// Lab 10
+
+// Global Variables
+var returnedNum;
+
+
+// Function
+function quickPick()
+{
+    var pOrF = prompt("Would you like a Florida ticket or Powerball ticket? \nType 1 for Florida or 2 for Powerball");
+
+
+}
+
+
+// Main code
+returnedNum = quickPick();
