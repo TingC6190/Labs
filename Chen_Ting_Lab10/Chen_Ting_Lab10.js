cc46bcb5ee392ca7a66002ad553a07e0d95b5721
TingC6190 <tingc6190@fullsail.edu>
2015-05-26 13:40:37 -0400

    Lab 10 Commit #5

--- Chen_Ting_Lab10/Chen_Ting_Lab10.js
+++ Chen_Ting_Lab10/Chen_Ting_Lab10.js
@@ -6,18 +6,19 @@
 
 // Global Variables
 var returnedNum;
-var pOrF = prompt("Would you like a Florida ticket or a Powerball ticket? \nType Florida or Powerball", "Powerball");
+var powerball;
+var pOrF = prompt("Would you like a Florida ticket or a Powerball ticket? \nType Florida or Powerball", "Florida or Powerball");
 
 // Function
 function quickPick(argTicket)
 {
     var arrayTicket = [];
-    // var randFlorida = Math.floor(Math.random() * (59 - 1) + 1);
     var hold;
 
     // Florida ticket code
     if (pOrF == "Florida")
     {
+        // loop through 6 times to get each random number
         for (var i = 0; i < 6; i++)
         {
             arrayTicket.push(Math.floor(Math.random() * (53 - 1) + 1));
@@ -28,16 +29,22 @@ function quickPick(argTicket)
     // Powerball ticket code
     else if (pOrF == "Powerball")
     {
+        // loop through 5 times to get each random number
         for (var i = 0; i < 5; i++)
         {
             arrayTicket.push(Math.floor(Math.random() * (59 - 1) + 1));
         }
         return arrayTicket;
     }
-    return arrayTicket;
 }
-
 
 // Main code
 returnedNum = quickPick(pOrF);
-console.log("Your " + pOrF + " ticket numbers are: " + returnedNum);
+console.log("The " + pOrF + " ticket numbers are: " + returnedNum);
+
+if (pOrF == "Powerball")
+{
+    // random number for powerball
+    powerball = Math.floor(Math.random() * (35 - 1) + 1);
+    console.log("Your Powerball is " + powerball);
+}
